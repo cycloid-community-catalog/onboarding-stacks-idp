7bc568ed884ac2ae1233a7d078facd9bcd1a64c7
Olivier de Turckheim <olivier.deturckheim@cycloid.io>
2025-07-21 11:54:38 +0200

    minor fix

--- stack-scaffold-demo/terraform/azuredevops/provider.tf
+++ stack-scaffold-demo/terraform/azuredevops/provider.tf
@@ -8,6 +8,5 @@ provider "azuredevops" {
   org_service_url = "https://dev.azure.com/${var.azuredevops_org}"
   client_id       = var.azure_cred["client_id"]
   client_secret   = var.azure_cred["client_secret"]
-  subscription_id = var.azure_cred["subscription_id"]
   tenant_id       = var.azure_cred["tenant_id"]
 }
