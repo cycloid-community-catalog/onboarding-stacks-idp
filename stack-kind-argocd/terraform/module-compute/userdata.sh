d3c5b9aceac7195f9af0540a5695ed961146222b
Olivier de Turckheim <olivier.deturckheim@cycloid.io>
2025-07-29 14:37:01 +0200

    minor fix

--- stack-kind-argocd/terraform/module-compute/userdata.sh
+++ stack-kind-argocd/terraform/module-compute/userdata.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 # To install SSM Agent on Debian Server
 # https://docs.aws.amazon.com/systems-manager/latest/userguide/agent-install-deb.html
+set -x
 mkdir /tmp/ssm
 cd /tmp/ssm
 wget https://s3.amazonaws.com/ec2-downloads-windows/SSMAgent/latest/debian_amd64/amazon-ssm-agent.deb
