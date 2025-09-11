b029cbdce67bdbb625cabc565e28ac61677f7cfa
Olivier de Turckheim <olivier.deturckheim@cycloid.io>
2025-09-11 13:14:17 +0200

    minor changes

--- stack-kind-argocd/terraform/git.tf
+++ stack-kind-argocd/terraform/git.tf
@@ -1,6 +1,6 @@
-resource "github_repository" "argocd" {
-  name        = "${var.cy_org}-${var.cy_project}-argocd"
-  description = "ArgoCD repo managed by Cycloid in ${var.cy_project} project in ${var.cy_org} org"
+resource "github_repository" "manifests" {
+  name        = "${var.cy_project}-${var.cy_env}-manifests"
+  description = "ArgoCD Manifests repo managed by Cycloid in ${var.cy_project} project in ${var.cy_org} org"
 
   visibility = "private"
   auto_init  = true
@@ -10,18 +10,18 @@ resource "tls_private_key" "github_generated_key" {
   algorithm   = "ED25519"
 }
 
-resource "github_repository_deploy_key" "argocd" {
-  title      = "${var.cy_org}-${var.cy_project}-argocd"
-  repository = github_repository.argocd.name
+resource "github_repository_deploy_key" "manifests" {
+  title      = "${var.cy_project}-${var.cy_env}-manifests"
+  repository = github_repository.manifests.name
   key        = tls_private_key.github_generated_key.public_key_openssh
   read_only  = false
 }
 
 resource "cycloid_credential" "ssh_key" {
-  name                   = "${var.cy_org}-${var.cy_project}-argocd-git-ssh"
-  description            = "SSH Key Pair used to access ArgoCD GitHub repo."
-  path                   = "${var.cy_org}-${var.cy_project}-argocd-git-ssh"
-  canonical              = "${var.cy_org}-${var.cy_project}-argocd-git-ssh"
+  name                   = "${var.cy_project}-${var.cy_env}-manifests-git-ssh"
+  description            = "SSH Key Pair used to access ArgoCD Manifests GitHub repo."
+  path                   = "${var.cy_project}-${var.cy_env}-manifests-git-ssh"
+  canonical              = "${var.cy_project}-${var.cy_env}-manifests-git-ssh"
 
   type = "ssh"
   body = {
