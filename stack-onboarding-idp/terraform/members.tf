0d8bcf258a95bf9d845049d1b01fc79db9c47b98
Olivier de Turckheim <olivier.deturckheim@cycloid.io>
2025-10-22 23:06:16 +0100

    minor changes

--- stack-onboarding-idp/terraform/members.tf
+++ stack-onboarding-idp/terraform/members.tf
@@ -1,20 +1,20 @@
 resource "cycloid_organization_member" "tf_org_member_infrastructure" {
   for_each = toset(var.team_members_infrastructure)
   email = each.value
-  role_canonical = "infrastructure"
+  role_canonical = "default-no-permissions"
   organization_canonical = var.cy_child_org_canonical
 }
 
 resource "cycloid_organization_member" "tf_org_member_application" {
   for_each = toset(var.team_members_application)
   email = each.value
-  role_canonical = "application"
+  role_canonical = "default-no-permissions"
   organization_canonical = var.cy_child_org_canonical
 }
 
 resource "cycloid_organization_member" "tf_org_member_integration" {
   for_each = toset(var.team_members_integration)
   email = each.value
-  role_canonical = "integration"
+  role_canonical = "default-no-permissions"
   organization_canonical = var.cy_child_org_canonical
 }
