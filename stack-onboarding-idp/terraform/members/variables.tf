baded6615d8a7d42785aa2c373feaf3bb32dbfa4
Olivier de Turckheim <olivier.deturckheim@cycloid.io>
2025-10-22 16:46:41 +0100

    minor changes

--- stack-onboarding-idp/terraform/members/variables.tf
+++ stack-onboarding-idp/terraform/members/variables.tf
@@ -8,36 +8,19 @@ variable "cy_child_org_canonical" {
   description = "Cycloid child organization canonical name"
 }
 
-# AWS variables
-variable "aws_region" {
-  description = "AWS region where to create servers."
-  default     = "eu-west-1"
-}
-
-# GitHub variables
-variable "github_url_idp" {
-  description = "GitHub URL for the IDP stacks"
-  default     = "https://github.com/cycloid-community-catalog/onboarding-stacks-idp.git"
-}
-
-variable "github_branch_idp" {
-  description = "GitHub branch for the IDP stacks"
-  default     = "master"
-}
-
 # Members
-variable "team_members_ccp_csp" {
-  description = "CCP/CSP team members to invite to the new software project."
+variable "team_members_infrastructure" {
+  description = "Infrastructure team members to invite to the new software project."
   type        = list(string)
 }
 
-variable "team_members_sods" {
-  description = "SODS team members to invite to the new software project."
+variable "team_members_application" {
+  description = "Application team members to invite to the new software project."
   type        = list(string)
 }
 
-variable "team_members_csds" {
-  description = "CSDS team members to invite to the new software project."
+variable "team_members_integration" {
+  description = "Integration team members to invite to the new software project."
   type        = list(string)
 }
 
