256664124133a3db9384a892e4feb6258d0b07ae
Olivier de Turckheim <olivier.deturckheim@cycloid.io>
2025-12-16 13:00:20 +0100

    minor change

--- stack-k8s-argocd/terraform/module-compute/dns.tf
+++ stack-k8s-argocd/terraform/module-compute/dns.tf
@@ -6,7 +6,7 @@ data "aws_route53_zone" "zone" {
 
 # Route53 wildcard A record for PR environments and argocd GUI (matches pr1.org-project-env.cycloid-demo.com and argocd.org-project-env.cycloid-demo.com)
 resource "aws_route53_record" "cycloid-demo" {
-  zone_id = data.aws_route53_zone.cycloid_demo.zone_id
+  zone_id = data.aws_route53_zone.zone.zone_id
   name    = "*.${var.cy_org}-${var.cy_project}-${var.cy_env}.${var.vm_aws_route53_zone}"
   type    = "A"
   ttl     = 300
