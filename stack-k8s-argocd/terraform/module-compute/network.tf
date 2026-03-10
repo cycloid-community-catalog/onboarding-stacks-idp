5d5ce1da3aca8cec252b6d471612ea70f3a7c68f
Olivier de Turckheim <olivier.deturckheim@cycloid.io>
2026-03-10 18:12:29 +0100

    Update VPC module version to 5.21.0 in network.tf

--- stack-k8s-argocd/terraform/module-compute/network.tf
+++ stack-k8s-argocd/terraform/module-compute/network.tf
@@ -1,6 +1,6 @@
 module "vpc" {
   source  = "terraform-aws-modules/vpc/aws"
-  version = "~> 5.19.0"
+  version = "~> 5.21.0"
 
   name = "${var.cy_org}-${var.cy_project}-${var.cy_env}-${var.cy_component}"
   
