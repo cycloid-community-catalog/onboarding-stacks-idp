4274d0218c0b5c8e7c1a4163fab3c4a25c15e832
Olivier de Turckheim <olivier.deturckheim@cycloid.io>
2026-03-10 17:03:16 +0100

    update cycloid provider auth

--- stack-onboarding-idp/terraform/provider.tf
+++ stack-onboarding-idp/terraform/provider.tf
@@ -22,6 +22,6 @@ provider "github" {
 
 provider "cycloid" {
   organization_canonical = var.cy_org
-  jwt                    = var.cy_api_key
+  api_key                = var.cy_api_key
   url                    = var.cy_api_url
 }
