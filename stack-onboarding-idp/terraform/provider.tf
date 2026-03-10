059b44394cff984bc122b8cdb09a6451e04938b3
Olivier de Turckheim <olivier.deturckheim@cycloid.io>
2026-03-10 18:37:39 +0100

    minor fix

--- stack-onboarding-idp/terraform/provider.tf
+++ stack-onboarding-idp/terraform/provider.tf
@@ -21,7 +21,7 @@ provider "github" {
 }
 
 provider "cycloid" {
-  organization_canonical = var.cy_org
-  api_key                = var.cy_api_key
-  url                    = var.cy_api_url
+  default_organization = var.cy_org
+  api_key              = var.cy_api_key
+  api_url              = var.cy_api_url
 }
