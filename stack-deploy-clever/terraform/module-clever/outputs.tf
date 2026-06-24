faa6e62552c6d800157765e5ef50537b806573d8
Olivier de Turckheim <olivier.deturckheim@cycloid.io>
2026-06-24 10:30:22 +0200

    Add application URL output to Clever Cloud Terraform configuration
    Enhanced the Terraform outputs for Clever Cloud by adding a new output for the public HTTPS URL of the Docker application. This update provides users with direct access to the application URL, improving usability and deployment clarity.

--- stack-deploy-clever/terraform/module-clever/outputs.tf
+++ stack-deploy-clever/terraform/module-clever/outputs.tf
@@ -7,3 +7,8 @@ output "docker_application_id" {
   description = "The ID of the Docker application."
   value       = clevercloud_docker.app_docker.id
 }
+
+output "application_url" {
+  description = "Public HTTPS URL of the Docker application (e.g. org-component-env-project.cleverapps.io)."
+  value       = "https://${local.app_fqdn}"
+}
--- stack-deploy-clever/terraform/outputs.tf
+++ stack-deploy-clever/terraform/outputs.tf
@@ -7,3 +7,8 @@ output "docker_application_id" {
   description = "The ID of the Docker application."
   value       = module.clever_cloud.docker_application_id
 }
+
+output "application_url" {
+  description = "Public HTTPS URL of the Docker application (e.g. org-component-env-project.cleverapps.io)."
+  value       = module.clever_cloud.application_url
+}
